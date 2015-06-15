281b2ed352b8efa12401e3d2a0a0b9c516153a11
Adrian Perez <aperezdc@users.noreply.github.com>
2015-06-15 21:05:31 +0200

    Fix @beats calculation, HH:MM display, black bg
    This patch adds a second line showing the time in HH:MM format (grey on the Pebble Time),
    fixes the calculation of the @beats, and changes background to black.

--- src/main.c
+++ src/main.c
@@ -2,38 +2,62 @@
 
 #define LENGTH_OF(v)  ((sizeof (v) / sizeof(v[0])))
     
-static Window *s_main_window = NULL;
-static TextLayer *s_time_layer = NULL;
+static Window    *s_main_window    = NULL;
+static TextLayer *s_beats_layer    = NULL;
+static TextLayer *s_time_layer     = NULL;
+static char       s_beats_buffer[] = "@000";
+static char       s_time_buffer[]  = "HH:MM";
 
 
 static void
 update_time (struct tm *localtm,
              TimeUnits  units_changed)
 {
+    /* We'll do all calculations starting from the UTC time. */
+    time_t utctime = mktime (localtm);
+    
+    /* The normal HH:MM text is straightforward. */
+    struct tm *T = localtime (&utctime);
+    strftime (s_time_buffer, LENGTH_OF (s_time_buffer), "%H:%M", T);
+    layer_mark_dirty (text_layer_get_layer (s_time_layer));
+
     /*
      * Convert the "struct tm" back to a time_t (seconds from the Epoch), so
      * it is possible to manipulate artifically the timezone, because we cannot
      * set TZ in the environment we have to do our own timezone adjustment :-/
      */
-    time_t utctime = mktime (localtm) + (60 * 60);   /* TZ=UTC-1 */
-    struct tm *T = gmtime (&utctime);
+    utctime -= (60 * 60);   /* TZ=UTC-1 */
+    T = gmtime (&utctime);
     
-    static char buffer[] = "@nnn";
-    snprintf (buffer, LENGTH_OF (buffer), "@%03u",
+    snprintf (s_beats_buffer, LENGTH_OF (s_beats_buffer), "@%03u",
               ((T->tm_sec) + (T->tm_min * 60) + (T->tm_hour * 3600)) * 1000 / 864);
-    text_layer_set_text (s_time_layer, buffer);
+    layer_mark_dirty (text_layer_get_layer (s_beats_layer));
 }
 
 
 static void
 main_window_load (Window *window)
 {
-    s_time_layer = text_layer_create (GRect (0, 55, 144, 50));
-    text_layer_set_background_color (s_time_layer, GColorClear);
-    text_layer_set_text_color (s_time_layer, GColorBlack);
-    text_layer_set_text (s_time_layer, "@000");
-    text_layer_set_font (s_time_layer,
+    window_set_background_color (s_main_window, GColorBlack);
+    
+    /* Beats */
+    s_beats_layer = text_layer_create (GRect (0, 55, 144, 50));
+    text_layer_set_background_color (s_beats_layer, GColorClear);
+    text_layer_set_text_color (s_beats_layer, GColorWhite);
+    text_layer_set_text (s_beats_layer, s_beats_buffer);
+    text_layer_set_font (s_beats_layer,
                          fonts_get_system_font (FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_alignment (s_beats_layer, GTextAlignmentCenter);
+    layer_add_child (window_get_root_layer (s_main_window),
+                     text_layer_get_layer (s_beats_layer));
+    
+    /* Normal HH:MM display */
+    s_time_layer = text_layer_create (GRect (0, 120, 144, 140));
+    text_layer_set_background_color (s_time_layer, GColorClear);
+    text_layer_set_text_color (s_time_layer,
+                               COLOR_FALLBACK (GColorLightGray,
+                                               GColorWhite));
+    text_layer_set_text (s_time_layer, s_time_buffer);
     text_layer_set_text_alignment (s_time_layer, GTextAlignmentCenter);
     layer_add_child (window_get_root_layer (s_main_window),
                      text_layer_get_layer (s_time_layer));
@@ -42,6 +66,7 @@ main_window_load (Window *window)
 static void
 main_window_unload (Window *window)
 {
+    text_layer_destroy (s_beats_layer);
     text_layer_destroy (s_time_layer);
 }
 
@@ -62,4 +87,5 @@ int main (int argc, char *argv[])
     app_event_loop ();
     
     window_destroy (s_main_window);
+    return 0;
 }
